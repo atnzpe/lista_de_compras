83fa59353b42fbcd7a35dd2bde13b75cc8acafaa
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-08-30 17:47:49 -0300

    Cria interaçõa com o usuario e lista vazia

---
+++ lista_de_compras.py
@@ -0,0 +1,13 @@
+# Pede o nome do usuário
+nome_usuario = input('Digite seu nome: ')
+# Pede o nome da lista de compras
+#nome_da_lista = input(f'Olá, {nome_usuario}, insira um nome para sua lista de compras: ')
+# Cria a  a lista
+lista_compras = []
+
+
+
+
+
+
+   
