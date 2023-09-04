5539f12c42ae7001e80fcfd239116a658695cbc9
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-09-04 11:32:34 -0300

    inclui a limpeza de tela dentro da função incluir_item

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -12,17 +12,21 @@ lista_compras = []
 
 
 def incluir_item():
+    # Limpar a tela
+    os.system('cls')
     # Cria uma nova variavel chamada novo_elemento
     novo_elemento = input('Insira a descrição do novo item: ')
     # Executa o metodo append e inclui um novo elemento na lista
     lista_compras.append(novo_elemento)
     # Envia uma mensagem na tela informando que o item foi gravado com sucesso
-    print(f'{novo_elemento=} inserido com com sucesso!')
+    print(f'{nome_usuario} o item {novo_elemento} foi inserido na lista!\n')
 
-# Funçõa Listar Itens e seus indices
+# Função Listar Itens e seus indices
 
 
 def listar_itens():
+    # Limpar a tela
+    os.system('cls')
     print('listando os objetos de uma lista\n')
 
     if len(lista_compras) == 0:
@@ -35,6 +39,8 @@ def listar_itens():
 
 
 def apagar_item():
+    # Limpar a tela
+    os.system('cls')
     # Pergunta ao usuario o que quer apagar
     print(f'Olá{nome_usuario}! Selecione o que quer apagar : \n')
     # cria a variavel opcao_apagar
@@ -64,12 +70,12 @@ def apagar_item():
 
 # Cria laço While para interação com o usuário
 while True:
-    
+
     print(f'Olá {nome_usuario}! Selecione uma opção: \n')
     resposta = input("[i]nserir, [a]pagar, [l]istar ou [s]air? ").lower()
 
     if 'i' in resposta:
-        os.system('cls')
+
         incluir_item()
         continue
 
@@ -82,14 +88,13 @@ while True:
         os.system('cls')
         listar_itens()
         continue
-    
+
     elif 's' in resposta:
         break
 
     else:
-        continue    
+        continue
 
 os.system('cls')
-print (f'Até mais {nome_usuario} Sua lista de itens esta logo abaixo :P')
+print(f'Até mais {nome_usuario} Sua lista de itens esta logo abaixo :P')
 listar_itens()
-
