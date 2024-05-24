d0b9e8299e98381f922384a3d95f81a70b723785
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-05-24 09:51:10 -0300

    feat: Implementada a funcionalidade de edição de itens
    Adicionada a capacidade de editar itens existentes na lista de compras.
    
    - Adicionado um botão Editar Item à tela principal.
    - Criado um modal para edição de itens, permitindo ao usuário:
        - Selecionar o item a ser editado através de um dropdown.
        - Alterar o nome, quantidade e valor unitário do item.
    - Implementada a lógica para salvar as alterações do item na lista de compras.
    - Habilitado o botão Editar Item após o login do usuário.
    
    Essa funcionalidade aumenta a flexibilidade do aplicativo, permitindo que os usuários corrijam ou modifiquem os itens da sua lista de compras de forma conveniente.

--- lista_de_compras_em_flet.py
+++ lista_de_compras_em_flet.py
@@ -1,3 +1,4 @@
+
 import os
 import datetime
 from reportlab.lib.pagesizes import letter
@@ -28,7 +29,7 @@ class Listadecompra:
         self.produtos = []  # Corrigido: de self.produto para self.produtos
         self.clientes = []
 
-    def criar_usuario(self, nome, cpf, forma_pagamento, endereco,email):
+    def criar_usuario(self, nome, cpf, forma_pagamento, endereco, email):
         """ "
         Cria um novo usuário e o adiciona à lista de usuários do banco.
 
@@ -46,7 +47,7 @@ class Listadecompra:
         """
         if self.obter_usuario_por_cpf(cpf):
             raise ValueError("Usuário já cadastrado com esse CPF.")
-        cliente = Cliente(nome, cpf, forma_pagamento, endereco,email)
+        cliente = Cliente(nome, cpf, forma_pagamento, endereco, email)
         self.clientes.append(cliente)
         return cliente
 
@@ -65,7 +66,7 @@ class Listadecompra:
                 return cliente
         return None
 
-    def incluir_produto(self, nome, quantidade, valor_unitario,valor_total):
+    def incluir_produto(self, nome, quantidade, valor_unitario, valor_total):
         """
         Inclui um novo produto à lista de produtos.
 
@@ -74,8 +75,9 @@ class Listadecompra:
             quantidade (float): Quantidade do item.
             valor (float): Valor unitário do item.
         """
-        produto = Produto(nome, quantidade, valor_unitario,valor_total)
-        self.produtos.append(produto)  # Corrigido: adicionado à lista self.produtos
+        produto = Produto(nome, quantidade, valor_unitario, valor_total)
+        # Corrigido: adicionado à lista self.produtos
+        self.produtos.append(produto)
 
     def listar_produtos(self):
         """Imprime na tela a lista de produtos com seus nomes, quantidades e valores."""
@@ -165,13 +167,15 @@ class Listadecompra:
         story = []
         story.append(Paragraph("Confirmação de Compra", styles["Heading1"]))
         story.append(Spacer(1, 12))
-        
+
         story.append(Paragraph("Dados do Comparador", styles["Heading2"]))
         story.append(Spacer(1, 12))
 
         story.append(Paragraph(f"Nome: {usuario.nome}", styles["Normal"]))
-        story.append(Paragraph(f"Endereço de Entrega: {usuario.endereco}", styles["Normal"]))
-        story.append(Paragraph(f"Forma de Pagamento: {usuario.forma_pagamento}", styles["Normal"]))
+        story.append(
+            Paragraph(f"Endereço de Entrega: {usuario.endereco}", styles["Normal"]))
+        story.append(
+            Paragraph(f"Forma de Pagamento: {usuario.forma_pagamento}", styles["Normal"]))
         story.append(Spacer(1, 12))
 
         story.append(Paragraph("Itens Comprados:", styles["Heading2"]))
@@ -229,13 +233,14 @@ class Listadecompra:
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as servidor:
             servidor.login(remetente_email, remetente_senha)
-            servidor.sendmail(remetente_email, usuario.email, mensagem.as_string())
+            servidor.sendmail(remetente_email, usuario.email,
+                              mensagem.as_string())
 
         print("Email de confirmação enviado com sucesso!")
 
 
 class Produto:
-    def __init__(self, nome, quantidade, valor_unitario,valor_total):
+    def __init__(self, nome, quantidade, valor_unitario, valor_total):
         """
         Inicializa um novo objeto Produto.
 
@@ -251,8 +256,6 @@ class Produto:
         self.valor_total = valor_total
         self.valor_unitario = valor_unitario
 
-    
-
 
 class Cliente:
     """
@@ -266,7 +269,7 @@ class Cliente:
         email (str): Endereço de email do cliente.  <-- Adicionado!
     """
 
-    def __init__(self, nome, cpf, forma_pagamento, endereco, email): 
+    def __init__(self, nome, cpf, forma_pagamento, endereco, email):
         """
         Inicializa um novo objeto Cliente.
 
@@ -281,7 +284,7 @@ class Cliente:
         self.cpf = cpf
         self.forma_pagamento = forma_pagamento
         self.endereco = endereco
-        self.email = email 
+        self.email = email
 
 
 class ListaComprasApp(ft.UserControl):
@@ -289,41 +292,46 @@ class ListaComprasApp(ft.UserControl):
         super().__init__()
         self.lista = Listadecompra()
         self.usuario_atual = None
-        
+
         # Define a tela inicial com botões
 
         self.view = ft.Column(
             [
-                ft.Text("Bem-vindo à Lista de Compras!", size=20),
-                ft.ElevatedButton("Login", on_click=self.abrir_modal_login),
-                ft.ElevatedButton("Incluir Item", on_click=self.abrir_modal_adicionar_item, disabled=True),
-                ft.ElevatedButton("Excluir Item", on_click=self.abrir_modal_apagar_item, disabled=True),
-                ft.ElevatedButton("Listar Produtos", on_click=self.abrir_modal_listar_produtos, disabled=True),
-                ft.ElevatedButton("Comprar Itens", on_click=self.abrir_modal_comprar_itens, disabled=True),
-                ft.ElevatedButton("Sair", on_click=self.sair_do_app),
+                ft.Text("Bem-vindo à Lista de Compras!", size=20),#Titulo
+                ft.ElevatedButton("Login", on_click=self.abrir_modal_login), #Botao [1]
+                ft.ElevatedButton(
+                    "Incluir Item", on_click=self.abrir_modal_adicionar_item, disabled=True),#Botao [2]
+                ft.ElevatedButton(
+                    "Excluir Item", on_click=self.abrir_modal_apagar_item, disabled=True),#Botao [3]
+                ft.ElevatedButton(
+                    "Listar Produtos", on_click=self.abrir_modal_listar_produtos, disabled=True),#Botao [4]
+                ft.ElevatedButton(
+                    "Editar Item",on_click=self.abrir_modal_editar_item,disabled=True),#Botao [5]
+                ft.ElevatedButton(
+                    "Comprar Itens", on_click=self.abrir_modal_comprar_itens, disabled=True),#Botao [6]
+                ft.ElevatedButton("Sair", on_click=self.sair_do_app),#Botao [7]
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
 
-        
-        
     def build(self):
         return self.view
-    
+
     def atualizar_estado_botoes(self):
         """Atualiza o estado dos botões com base no login."""
         logado = bool(self.usuario_atual)  # True se usuario_atual não for None
-        
+
         if logado:
             self.view.controls[1].disabled = True
-        
+
         self.view.controls[2].disabled = not logado  # Incluir Item
-        self.view.controls[3].disabled = not logado  # Excluir Item 
+        self.view.controls[3].disabled = not logado  # Excluir Item
         self.view.controls[4].disabled = not logado  # Listar Produtos
-        self.view.controls[5].disabled = not logado  # Comprar Itens 
+        self.view.controls[5].disabled = not logado  # Editar Itens
+        self.view.controls[6].disabled = not logado  # Comprar Itens
 
         # Atualiza a view para refletir as mudanças
-        self.view.update()     
+        self.view.update()
 
     def abrir_modal_login(self, e):
         self.dlg_login = ft.AlertDialog(
@@ -333,8 +341,10 @@ class ListaComprasApp(ft.UserControl):
                 [
                     ft.TextField(label="Digite seu Nome", ref=ft.Ref[str]()),
                     ft.TextField(label="CPF", ref=ft.Ref[str]()),
-                    ft.TextField(label="Forma de Pagamento", ref=ft.Ref[str]()),
-                    ft.TextField(label="Informe o endereço (logradouro, nro - bairro - cidade/sigla estado)", ref=ft.Ref[str]()),
+                    ft.TextField(label="Forma de Pagamento",
+                                 ref=ft.Ref[str]()),
+                    ft.TextField(
+                        label="Informe o endereço (logradouro, nro - bairro - cidade/sigla estado)", ref=ft.Ref[str]()),
                     ft.TextField(label="Email", ref=ft.Ref[str]()),
                 ]
             ),
@@ -347,7 +357,6 @@ class ListaComprasApp(ft.UserControl):
         self.page.dialog = self.dlg_login
         self.dlg_login.open = True
         self.page.update()
-    
 
     def fazer_login(self, e):
         dlg = self.page.dialog
@@ -358,11 +367,12 @@ class ListaComprasApp(ft.UserControl):
         email = dlg.content.controls[4].value
 
         try:
-            self.usuario_atual = self.lista.criar_usuario(nome, cpf, forma_pagamento, endereco, email)
+            self.usuario_atual = self.lista.criar_usuario(
+                nome, cpf, forma_pagamento, endereco, email)
             self.fechar_modal(e)  # Fecha o modal de login
             # Atualiza o estado dos botões
             self.atualizar_estado_botoes()
-            
+
         except ValueError as e:
             print(f"Erro no login: {e}")
             # Aqui você pode adicionar lógica para exibir a mensagem de erro no modal, se desejar
@@ -373,9 +383,12 @@ class ListaComprasApp(ft.UserControl):
             title=ft.Text("Adicionar Item"),
             content=ft.Column(
                 [
-                    ft.TextField(label="Digite o Nome do Produto", ref=ft.Ref[str]()),
-                    ft.TextField(label="Ditie a Quantidade", ref=ft.Ref[str]()),
-                    ft.TextField(label="Digite o Valor Unitário", ref=ft.Ref[str]()),
+                    ft.TextField(label="Digite o Nome do Produto",
+                                 ref=ft.Ref[str]()),
+                    ft.TextField(label="Ditie a Quantidade",
+                                 ref=ft.Ref[str]()),
+                    ft.TextField(label="Digite o Valor Unitário",
+                                 ref=ft.Ref[str]()),
                 ]
             ),
             actions=[
@@ -399,20 +412,20 @@ class ListaComprasApp(ft.UserControl):
                 quantidade = float(quantidade)
                 valor_unitario = float(valor_unitario)
                 valor_total = quantidade * valor_unitario
-                self.lista.incluir_produto(nome, quantidade, valor_unitario,valor_total)
+                self.lista.incluir_produto(
+                    nome, quantidade, valor_unitario, valor_total)
                 self.fechar_modal(e)  # Fecha o modal
             except ValueError:
                 print("Digite valores numéricos válidos.")
         else:
             print("Você precisa fazer login primeiro.")
 
-    # ... (Funções para outros modais: abrir_modal_apagar_item, abrir_modal_listar_produtos, etc.)
 
     def abrir_modal_comprar_itens(self, e):
         if not self.lista.produtos:
             self.mostrar_alerta("Sua lista de compras está vazia!")
             return
-        
+
         # Cria a lista de itens para o modal
         itens_lista = [
             ft.ListTile(
@@ -425,8 +438,9 @@ class ListaComprasApp(ft.UserControl):
         ]
 
         # Calcula o valor total da compra
-        valor_total = sum(produto.valor_total for produto in self.lista.produtos)
-        
+        valor_total = sum(
+            produto.valor_total for produto in self.lista.produtos)
+
         # Cria o modal de confirmação de compra
         self.dlg_comprar_itens = ft.AlertDialog(
             modal=True,
@@ -435,7 +449,8 @@ class ListaComprasApp(ft.UserControl):
                 [
                     ft.Text("Itens da sua compra:"),
                     ft.Column(itens_lista),
-                    ft.Text(f"Valor Total: R$ {valor_total:.2f}", size=16, weight=ft.FontWeight.BOLD),
+                    ft.Text(
+                        f"Valor Total: R$ {valor_total:.2f}", size=16, weight=ft.FontWeight.BOLD),
                 ]
             ),
             actions=[
@@ -462,7 +477,7 @@ class ListaComprasApp(ft.UserControl):
 
     def sair_do_app(self, e):
         self.page.window_destroy()
-        
+
     def abrir_modal_apagar_item(self, e):
         """Abre o modal para apagar um item."""
         if not self.lista.produtos:
@@ -484,12 +499,14 @@ class ListaComprasApp(ft.UserControl):
                         ),
                         ref=ft.Ref[ft.RadioGroup](),
                     ),
-                    ft.TextField(label="Índice ou Nome", ref=ft.Ref[ft.TextField]()),
+                    ft.TextField(label="Índice ou Nome",
+                                 ref=ft.Ref[ft.TextField]()),
                 ]
             ),
             actions=[
                 ft.TextButton("Cancelar", on_click=self.fechar_modal),
-                ft.ElevatedButton("Apagar", on_click=self.confirmar_apagar_item),
+                ft.ElevatedButton(
+                    "Apagar", on_click=self.confirmar_apagar_item),
             ],
             actions_alignment=ft.MainAxisAlignment.END,
         )
@@ -536,7 +553,7 @@ class ListaComprasApp(ft.UserControl):
             ft.ListTile(
                 title=ft.Text(produto.nome),
                 subtitle=ft.Text(
-                    f"Quantidade: {produto.quantidade}, Valor Total do Item: {produto.valor:.2f}"
+                    f"- Quantidade: {produto.quantidade}, Valor unitário do item: {produto.valor_unitario:.2f} Valor Total do Item: {produto.valor_total:.2f}"
                 ),
             )
             for produto in self.lista.produtos
@@ -555,12 +572,73 @@ class ListaComprasApp(ft.UserControl):
         dlg_listar_produtos.open = True
         self.page.update()
 
-    def mostrar_alerta(self, mensagem):
+    def abrir_modal_editar_item(self, e):
+    
+        """Abre o modal para editar um item da lista."""
+        if not self.lista.produtos:
+            self.mostrar_alerta("Sua lista de compras está vazia!")
+            return
+
+        self.dlg_editar_item = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Editar Item"),
+            content=ft.Column(
+                [
+                    ft.Text("Escolha o item a ser editado:"),
+                    ft.Dropdown(
+                        width=300,
+                        options=[
+                            ft.dropdown.Option(produto.nome) for produto in self.lista.produtos
+                        ],
+                        ref=ft.Ref[ft.Dropdown](),
+                    ),
+                    ft.TextField(label="Novo Nome", ref=ft.Ref[ft.TextField]()),
+                    ft.TextField(label="Nova Quantidade", ref=ft.Ref[ft.TextField]()),
+                    ft.TextField(label="Novo Valor Unitário", ref=ft.Ref[ft.TextField]()),
+                ]
+            ),
+            actions=[
+                ft.TextButton("Cancelar", on_click=self.fechar_modal),
+                ft.ElevatedButton("Salvar", on_click=self.salvar_edicao_item),
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+        )
+        self.page.dialog = self.dlg_editar_item
+        self.dlg_editar_item.open = True
+        self.page.update()
+    '''def mostrar_alerta(self, mensagem):
         """Exibe um alerta (SnackBar) na tela."""
         self.page.snack_bar = ft.SnackBar(ft.Text(mensagem))
         self.page.snack_bar.open = True
         self.page.update()
-        
+    '''
+    def salvar_edicao_item(self, e):
+        """Salva as alterações feitas no item."""
+        dlg = self.page.dialog
+        nome_item_selecionado = dlg.content.controls[1].value
+        novo_nome = dlg.content.controls[2].value
+        nova_quantidade = dlg.content.controls[3].value
+        novo_valor_unitario = dlg.content.controls[4].value
+
+        try:
+            nova_quantidade = float(nova_quantidade) if nova_quantidade else None
+            novo_valor_unitario = float(novo_valor_unitario) if novo_valor_unitario else None
+
+            for i, produto in enumerate(self.lista.produtos):
+                if produto.nome == nome_item_selecionado:
+                    if novo_nome:
+                        self.lista.produtos[i].nome = novo_nome
+                    if nova_quantidade:
+                        self.lista.produtos[i].quantidade = nova_quantidade
+                    if novo_valor_unitario:
+                        self.lista.produtos[i].valor = nova_quantidade * novo_valor_unitario  # Recalcula o valor total
+                    break
+
+            self.fechar_modal(e)
+            self.mostrar_alerta("Item editado com sucesso!")
+        except ValueError:
+            self.mostrar_alerta("Digite valores numéricos válidos para Quantidade e Valor Unitário.")
+    
     def mostrar_alerta(self, mensagem):
         """Exibe um alerta (SnackBar) na parte CENTRAL da tela."""
         self.page.snack_bar = ft.SnackBar(
@@ -571,10 +649,11 @@ class ListaComprasApp(ft.UserControl):
                 border_radius=ft.border_radius.all(5),
             ),
             # Define a posição horizontal como center
-            action="OK", 
+            action="OK",
         )
         self.page.snack_bar.open = True
         self.page.update()
+
 
 def main(page: ft.Page):
     page.title = "Lista de Compras"
@@ -582,5 +661,6 @@ def main(page: ft.Page):
     app = ListaComprasApp()
     page.add(app)
     app.atualizar_estado_botoes()
+
 
 ft.app(target=main)
